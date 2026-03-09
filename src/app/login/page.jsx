2d7899eab94053eb33bd9e88d34024fd6670cabe
vinod <vinod@devqsoft.com>
2026-03-09 17:44:28 +0530

    fix login page blank component

---
+++ src/app/login/page.jsx
@@ -0,0 +1,11 @@
+// app/login/page.tsx
+import React from "react";
+
+export default function Login() {
+  return (
+    <main>
+      <h1>Login</h1>
+      {/* login form / UI */}
+    </main>
+  );
+}
