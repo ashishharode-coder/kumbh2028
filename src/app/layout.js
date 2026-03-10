7e41618225ed2df2ca3bad1bb5b212c00a66e7db
vinod <vinod@devqsoft.com>
2026-03-10 14:36:22 +0530

    Counter UI

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/layout/Navbar";
-import Footer from "@/components/layout/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,14 +25,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        
-        
-        {/* <Navbar /> */}
-
-        {/* Page Content */}
         {children}
-
-        {/* <Footer/> */}
 
       </body>
     </html>
--- src/app/loading.js
+++
@@ -1,6 +0,0 @@
-import Loader from "@/components/ui/Loader";
-
-export default function Loading() {
-    // Ye component tab dikhega jab Next.js routes ke beech navigate karega
-    return <Loader />;
-}
